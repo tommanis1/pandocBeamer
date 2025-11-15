6a60812c6c18f976badaf527795f3a1592c9b56e
Tommaso Pacciani <>
2025-11-15 12:00:33 +0100

    js shim for random in (haskell -> js)

---
+++ js/ghcjs-shims.js
@@ -0,0 +1,9 @@
+<script language="javascript">
+function h$splitmix_init() {
+    return [Math.floor(Math.random() * 2147483647), 0];
+}
+
+function h$splitmix_next() {
+    return [Math.floor(Math.random() * 2147483647), 0];
+}
+</script>
